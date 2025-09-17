21e0f28bfe37374c329a42f4b24fab4fe27a526e
sumu <2038035593@qq.com>
2025-09-17 20:17:12 +0800

    删除test测试目录

--- src/test/articlePage/aside-iframe.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { withBase } from "vitepress";
-</script>
-
-<template>
-  <iframe :src="withBase('/test/articlePage/aside')" width="100%" height="440" frameborder="0" />
-</template>
--- src/test/articlePage/doc-iframe.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { withBase } from "vitepress";
-</script>
-
-<template>
-  <iframe :src="withBase('/test/articlePage/doc')" width="100%" height="360" frameborder="0" />
-</template>
